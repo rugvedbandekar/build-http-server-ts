ac35f419e55265fd17986c5e6bd2f3da65d308e2
rugved97 <rugvedbandekar@gmail.com>
2024-07-16 20:33:29 +0530

    pass stage 2

--- app/main.ts
+++ app/main.ts
@@ -5,7 +5,18 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-  socket.write(`HTTP/1.1 200 OK\r\n\r\n`);
+  socket.on("data", (data) => {
+    const request = data.toString();
+    const path = request.split(" ")[1];
+    const response =
+      path === "/"
+        ? "HTTP/1.1 200 OK\r\n\r\n"
+        : "HTTP/1.1 404 Not Found\r\n\r\n";
+    console.log(response);
+    socket.write(response);
+    socket.end();
+  });
+
   socket.on("close", () => {
     socket.end();
   });
